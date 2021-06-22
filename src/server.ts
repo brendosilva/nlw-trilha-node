a8161a29c8ece9c108adb501371a0540a32d57e9
Brendo Silva <breendo.01@gmail.com>
2021-06-22 01:30:30 -0300

    criando rota users e controller

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+
 import "reflect-metadata";
 
+
 import "./database";
+import { router } from './router';
 
 const app = express();
+app.use(express.json());
+app.use(router);
 
 
 app.listen(3000, () => {
