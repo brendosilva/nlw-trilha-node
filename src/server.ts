d1114f4a89d23809b9312c51b361bab11a2616ce
Brendo Silva <breendo.01@gmail.com>
2021-06-25 01:23:24 -0300

    adicionando cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 import "express-async-errors";
+import cors from "cors";
 
 import "reflect-metadata";
 
@@ -8,6 +9,7 @@ import "./database";
 import { router } from './router';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
