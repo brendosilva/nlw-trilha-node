5417178fc1ad7cb72159c4d43725216c8072037c
Brendo Silva <breendo.01@gmail.com>
2021-06-25 01:01:52 -0300

    Adicionando biblioteca class-transformer

--- src/router.ts
+++ src/router.ts
@@ -5,18 +5,29 @@ import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { ensureAuthenticate } from "./middlewares/ensureAuthenticate";
+import { ListUserReceiverComplimentsController } from "./controllers/ListUserReceiverComplimentsController";
+import { ListUserSenderComplimentsController } from "./controllers/ListUserSenderComplimentsController";
+import { ListTagsController } from "./controllers/ListTagsController";
 
 const router = Router();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
+const listUserReceiverComplimentsController = new ListUserReceiverComplimentsController();
+const listUserSenderComplimentsController = new ListUserSenderComplimentsController();
+const listTagsControler = new ListTagsController();
 
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAuthenticate, ensureAdmin, createTagController.handle);
 router.post("/session", authenticateUserController.handle);
 router.post("/compliments", ensureAuthenticate, createComplimentController.handle);
 
+
+
+router.get("/users/compliments/sender", ensureAuthenticate, listUserReceiverComplimentsController.handle);
+router.get("/users/compliments/receiver", ensureAuthenticate, listUserSenderComplimentsController.handle);
+router.get("/tags", ensureAuthenticate, listTagsControler.handle);
 
 
 export { router };
